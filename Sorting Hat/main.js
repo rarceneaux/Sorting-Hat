1a495bd36fce8d1af26a8a1e6389e08c43ccbfd9
Raymond Arceneaux <raymond.techcoder@gmail.com>
2018-09-27 18:39:54 -0500

    sort button

--- Sorting Hat/main.js
+++ Sorting Hat/main.js
@@ -1,30 +1,72 @@
-// // console.log('Sorting Hat');
-// const houses = ['Gryffindor','Hufflepuff','Ravenclaw','Slytherin'];
+const houses = ['Gryffindor','Hufflepuff','Ravenclaw','Slytherin'];
 
-let getStartedButton = document.getElementById('getStarted');
-// console.log(getStartedButton);
+const getStartedButton = document.getElementById('getStarted');
 
 document.getElementById('getStarted').addEventListener ('click' , (event) => {
- getForm();
-});
+  getForm();
+})
 
 const printToDom = (stringToPrint, whereToPrint) => {
 document.getElementById(whereToPrint).innerHTML += stringToPrint;
 }
 
-// printToDom('hey' ,'form');
+// add delete functions
 
+const activateDeletes = () => {
+  const deleteButtons = document.getElementsById('expel');
+
+  for (let i = 0; i < deleteButtons.length; i++) {
+      const element = deleteButtons[i];
+      element.addEventListener("click", (e) => {
+          const buttonIClicked = e.target;
+          const cardToDelete = buttonIClicked.parentNode.parentNode;
+          cardToDelete.remove();
+      })
+  }
+}
+
+// form
 const getForm = () => {
     let nameForm = `<form class="form-inline">
-    <div class="form-group mb-2">
-      <label for="" class="sr-only">Email</label>
-      <input type="text" readonly class="form-control-plaintext" id="staticEmail2" value="Student">
+    <div  id='form' class="form-group mb-2">
+      <label for="studentName" id="studentName">Enter First Year's Name</label>
+      </div>
+      <div id="studentSection>
+      <label for="Name" id="Name">Student:</label>
+      </div>
+      <div>
+      <input type="text" id="studentNameInput"> 
+      </div>
     </div>
-    <div class="form-group mx-sm-3 mb-2">
-      <label for="" class="sr-only">Password</label>
-      <input type="text" class="form-control" id="inputPassword2" placeholder="">
+    <div>
+    <button type="submit" id="sort">Sort</button>
     </div>
-    <button type="submit" class="btn btn-primary mb-2">SORT!</button>
   </form>`;
-  printToDom(nameForm, 'form');
+  printToDom(nameForm, 'form');  
+  makeSortWork();
+
+}
+// // // sort button  2 Event Listener functionality 
+
+const addStudent = () => {
+  const studentInfo = studentNameInput.value.toLowerCase();
+  let studentCard = "";
+  let housePicks = houses[Math.floor.random()*(houses.length)];
+      studentCard += `<div class="card" style="width: 18rem;">
+  <h1 class="studentName">${studentInfo}</h1>
+  <h3 class="houseName">${houses}</h3>
+  <div class="card-body">
+  <button type="submit" id="expel">Expel</button>
+  </div>
+</div>`;
+  }
+  printToDom(studentCard,'card');
+  
+}
+
+const makeSortWork = () =>{
+  document.getElementById('sort').addEventListener ('click' , (event) => {
+  event.preventDefault()
+  addStudent();
+})
 }
